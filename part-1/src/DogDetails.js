06372071b1717c122db992eaa1d38a3f26273bc6
Ryan <ryjen.pears@gmail.com>
2022-06-30 09:41:24 +0200

    dog details added

---
+++ part-1/src/DogDetails.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Link, Redirect } from 'react-router-dom';
+
+function DogDetails({ dog }) {
+  
+  if (!dog) return <Redirect to="/dogs" />
+  
+  return (
+    <div className='dogDetails'>
+      <img src={dog.src} alt={dog.name} />
+      <h1>{dog.name}</h1>
+      <h3>{dog.age} years old</h3>
+      <ul>
+        {dog.facts.map((fact, index) => (
+          <li key={index}>{fact}</li>
+        ))}
+      </ul>
+      <Link to="/dogs">Go back</Link>
+    </div>
+  );
+}
+
+export default DogDetails;
